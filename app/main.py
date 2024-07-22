c8d1921a781a078f6a060008ee672389f0c689d8
Surya Prakash Sonkar <91277661+suryapsnkr@users.noreply.github.com>
2024-07-22 22:44:42 +0530

    main.py

--- app/main.py
+++ app/main.py
@@ -17,17 +17,21 @@ async def register_user(user: schemas.UserCreate, db: Session = Depends(get_db))
     db_user = crud.create_user(db=db, user=user)
     return db_user
 
-@app.post("/profile/{user_id}")
-async def create_user_profile(user_id: int, file: UploadFile):
-    profile = await crud.get_profile(mongodb, user_id)
-    if profile:
-        raise HTTPException(status_code=404, detail="Profile already exists")
+@app.post("/profile/{user_id}", response_model=schemas.UserInDB)
+async def create_user_profile(user_id: int, file: UploadFile, db: Session = Depends(get_db)):
+    db_user = db.query(models.User).filter(models.User.id == user_id).first()
+    if db_user is None:
+        raise HTTPException(status_code=404, detail="User not found")
     else:
-        profile = "app/propics/"+file.filename
-        with open(profile, 'wb') as f:
-            shutil.copyfileobj(file.file, f)
-        profile_id = await crud.create_profile(mongodb, user_id, profile)
-        return {"profile_id": user_id, "profile_picture": profile}
+        profile = await crud.get_profile(mongodb, user_id)
+        if profile:
+            raise HTTPException(status_code=404, detail="Profile already exists")
+        else:
+            profile = "app/propics/"+file.filename
+            with open(profile, 'wb') as f:
+                shutil.copyfileobj(file.file, f)
+            profile_id = await crud.create_profile(mongodb, user_id, profile)
+            return {"profile_id": user_id, "profile_picture": profile}
 
 @app.get("/user/{user_id}", response_model=schemas.UserInDB)
 def read_user(user_id: int, db: Session = Depends(get_db)):
